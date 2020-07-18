6edd0a1f727bc11704294c9c4671d8fd795b3009
Dilanjer <artur_hovhannisyan@protonmail.com>
2020-07-18 18:59:48 +0400

    Updates

---
+++ precache-manifest.52d128f6a11f039b1fb8cecc4e690b00.js
@@ -0,0 +1,126 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "08d28f55b7fa1240f3358782bce804b9",
+    "url": "/wireframe/index.html"
+  },
+  {
+    "revision": "f9659fa93ce2757a4a20",
+    "url": "/wireframe/static/css/main.4ecef1f0.chunk.css"
+  },
+  {
+    "revision": "8620ff226f743d4651d8",
+    "url": "/wireframe/static/js/2.6dc2348e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/wireframe/static/js/2.6dc2348e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f9659fa93ce2757a4a20",
+    "url": "/wireframe/static/js/main.92c293c2.chunk.js"
+  },
+  {
+    "revision": "fd3413549d735cbb3136",
+    "url": "/wireframe/static/js/runtime-main.4668c2ac.js"
+  },
+  {
+    "revision": "137a9f5f920bcccad63c115397dc1532",
+    "url": "/wireframe/static/media/Montserrat-Bold.137a9f5f.woff"
+  },
+  {
+    "revision": "1eaf59537a9c317acaa4b2c144cd89d6",
+    "url": "/wireframe/static/media/Montserrat-Bold.1eaf5953.woff2"
+  },
+  {
+    "revision": "38a49c5d1b18bfb6a0b2379ec8c4354b",
+    "url": "/wireframe/static/media/Montserrat-Bold.38a49c5d.ttf"
+  },
+  {
+    "revision": "7ad9c1a987324c63923bdd6b4f58e87c",
+    "url": "/wireframe/static/media/Montserrat-Bold.7ad9c1a9.eot"
+  },
+  {
+    "revision": "228e8e006654fc7aa665f322090c56af",
+    "url": "/wireframe/static/media/Montserrat-Light.228e8e00.eot"
+  },
+  {
+    "revision": "40e31707c08e220773ea4b0bdf4f7919",
+    "url": "/wireframe/static/media/Montserrat-Light.40e31707.woff2"
+  },
+  {
+    "revision": "75ac2d463b1b7332a85e9bd0478567d2",
+    "url": "/wireframe/static/media/Montserrat-Light.75ac2d46.woff"
+  },
+  {
+    "revision": "91fa45ed0f53fe27ed2a2134b58307a1",
+    "url": "/wireframe/static/media/Montserrat-Light.91fa45ed.ttf"
+  },
+  {
+    "revision": "4183bb3edc82bb19a8ddf6573acbdcd8",
+    "url": "/wireframe/static/media/Montserrat-Medium.4183bb3e.eot"
+  },
+  {
+    "revision": "4193cb373574474bd262f0cada40c68e",
+    "url": "/wireframe/static/media/Montserrat-Medium.4193cb37.woff2"
+  },
+  {
+    "revision": "72aebf4516ddcbc7634c4640fb6f9a90",
+    "url": "/wireframe/static/media/Montserrat-Medium.72aebf45.woff"
+  },
+  {
+    "revision": "91c0784cecdbcb3badf85ba842de5f21",
+    "url": "/wireframe/static/media/Montserrat-Medium.91c0784c.ttf"
+  },
+  {
+    "revision": "5b3ad248f67d8b84cb14f4f35cd031e8",
+    "url": "/wireframe/static/media/Montserrat-Regular.5b3ad248.eot"
+  },
+  {
+    "revision": "6c8807219b0ecffdf96122b80df3e62c",
+    "url": "/wireframe/static/media/Montserrat-Regular.6c880721.woff2"
+  },
+  {
+    "revision": "afd7a5cb0ff90afa9eadd0996ad2da1e",
+    "url": "/wireframe/static/media/Montserrat-Regular.afd7a5cb.ttf"
+  },
+  {
+    "revision": "c0b8804ae85213c9194816da945db150",
+    "url": "/wireframe/static/media/Montserrat-Regular.c0b8804a.woff"
+  },
+  {
+    "revision": "60ebedd4fcfab4c919aee49af5717aa5",
+    "url": "/wireframe/static/media/Montserrat-SemiBold.60ebedd4.ttf"
+  },
+  {
+    "revision": "ab8ea2e0952cbe3792985696c2ed9fbf",
+    "url": "/wireframe/static/media/Montserrat-SemiBold.ab8ea2e0.eot"
+  },
+  {
+    "revision": "c74260cde1a16b48febba358704ac4b6",
+    "url": "/wireframe/static/media/Montserrat-SemiBold.c74260cd.woff2"
+  },
+  {
+    "revision": "f4103669740e1258103e7fee7bfafbc1",
+    "url": "/wireframe/static/media/Montserrat-SemiBold.f4103669.woff"
+  },
+  {
+    "revision": "6a8b967b12e4c7140882500d6d3ead0b",
+    "url": "/wireframe/static/media/burger-close.6a8b967b.svg"
+  },
+  {
+    "revision": "de2ccd19b8f275e3d2bfea16a026abc8",
+    "url": "/wireframe/static/media/burger-menu.de2ccd19.svg"
+  },
+  {
+    "revision": "6aa4ebcec887bb5e82d97d25e9b1b796",
+    "url": "/wireframe/static/media/ipad-air.6aa4ebce.png"
+  },
+  {
+    "revision": "76a122c28cedb1ba5c5192448865174a",
+    "url": "/wireframe/static/media/macbook-pro.76a122c2.png"
+  },
+  {
+    "revision": "c32bb29ee3533ff4f1f39d95377e8779",
+    "url": "/wireframe/static/media/mail2.c32bb29e.svg"
+  }
+]);
